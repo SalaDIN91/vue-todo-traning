9d652153855e8763d1590446e55770019c0352f1
saladin <bakhtiyev.damir@gmail.com>
2021-03-25 22:09:10 +0600

    Init route

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    This is Home Page
+    <task></task>
+  </div>
+</template>
+
+<script>
+import Task from "@/components/Task";
+export default {
+  name: "Home",
+  components: {Task},
+}
+
+</script>
+
+<style scoped>
+
+</style>
